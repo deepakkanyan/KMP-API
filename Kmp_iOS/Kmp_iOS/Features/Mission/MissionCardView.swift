c8bd24f86872de91d05b96d3155488269b910eb2
deepakkanyan <dpk.ror7@gmail.com>
2023-08-25 10:55:00 -0400

    Capsule Api structure

---
+++ Kmp_iOS/Kmp_iOS/Features/Mission/MissionCardView.swift
@@ -0,0 +1,16 @@
+//
+//  MissionCardView.swift
+//  Kmp_iOS
+//
+//  Created by Deepak Kanyan on 23/08/23.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct MissionCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
---
+++ Kmp_iOS/Kmp_iOS/Features/Mission/MissionViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  MissionViewModel.swift
+//  Kmp_iOS
+//
+//  Created by Deepak Kanyan on 23/08/23.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import Foundation
+
+
+class MissionViewModel : ObservableObject{
+    
+    
+    
+    
+    
+}
