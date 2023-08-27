4901478469c102c01ca439d76620602809c99fd5
deepakkanyan <dpk.ror7@gmail.com>
2023-08-27 16:54:24 -0400

    Mission Swift UI Design

--- Kmp_iOS/Kmp_iOS/Features/Mission/MissionViewModel.swift
+++ Kmp_iOS/Kmp_iOS/Features/Mission/MissionViewModel.swift
@@ -7,12 +7,40 @@
 //
 
 import Foundation
+import shared
 
 
 class MissionViewModel : ObservableObject{
     
+    @Published var missionResponse: KmpResponse<[MissionModel]>? // Nullable because Kotlin types are nullable
+        
+        init() {
+            fetchMissions()
+        }
+        
+        func fetchMissions() {
+            GetMissionListUseCase().invoke { kmpResponse,arg ,arg\\\  in // Replace with your actual method
+                
+                if let successResponse = kmpResponse as? SharedCode.KmpResponse.Success {
+                    self.missionResponse = .success(data: successResponse.data)
+                }
+                else if let errorResponse = kmpResponse as? SharedCode.KmpResponse.Error {
+                    self.missionResponse = .error(exception: errorResponse.exception)
+                }
+                else if kmpResponse is SharedCode.KmpResponse.Loading {
+                    self.missionResponse = .loading
+                }
+                else {
+                    // handle unexpected cases
+                }
+            }
+        }
+
     
-   // @Published var rocketResponse: KmpResponse = .Loading.shared
+    
+
+    
+    
     
     
 }
